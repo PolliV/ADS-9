fc781006c752978ce3ad9d9bc59263f73088d360
PolliV <77505747+PolliV@users.noreply.github.com>
2023-06-04 23:32:24 +0300

    b54 alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,22 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
+  BST<std::string> tree;
+  std::ifstream file(filename);
+  std::string fail;
+  while (!file.eof()) {
+    int mac = file.get();
+    if (mac >= 65 && mac <= 90) {
+      mac += 32;
+      fail += mac;
+    } else if (mac >= 97 && mac <= 122) {
+      fail += mac;
+    } else {
+      tree.add(fail);
+      fail = "";
+    }
+  }
+  file.close();
+  return tree;
   // поместите сюда свой код
 }
